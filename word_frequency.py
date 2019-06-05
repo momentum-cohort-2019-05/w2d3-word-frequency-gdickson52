552c12a22d3bce8b447fd8bf9ecc4a84315ca101
gdickson52 <gdickson52@gmail.com>
2019-06-05 17:47:53 -0400

    word_frequency.py

--- word_frequency.py
+++ word_frequency.py
@@ -1,13 +1,36 @@
+import re
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
 
+f = open ("seneca_falls.txt")
+all_text = (f.read())
+all_text = str(all_text)
+all_text = all_text.lower()
+all_text = re.sub('[^A-Za-z]', ' ', all_text) 
+print(all_text)
+all_text = all_text.split()
+word_text = []
+for words in all_text:
+    if words not in STOP_WORDS:
+        word_text.append(words)
+print(word_text)
 
 def print_word_freq(file):
-    """Read in `file` and print out the frequency of words in that file."""
     pass
+wordfreg = []
+for w in word_text:
+    all_text.append(word_text.count(w))
+print("string\n" , all_text , "\n")
+print("list\n" + str(word_text) + "\n")
+print("frequencies\n" + str(f) + "\n")
+print("pairs\n" + str(zip(word_text, f)))  
+
+
+
+
 
 
 if __name__ == "__main__":
